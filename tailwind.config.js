eb0b5ba1cba4f65a2c8dc0bb0a1e08eb586a565f
Julian Enrique <julian.buitrago3003@gmail.com>
2024-11-06 00:34:37 -0500

    add 700 red color

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,8 @@ module.exports = {
         50: '#fef2f2',
         100: '#fee2e2',
         500: '#ef4444',
-        600: '#dc2626'
+        600: '#dc2626',
+        700: '#b91c1c'
       },
       yellow: {
         50: '#fefcbf',
