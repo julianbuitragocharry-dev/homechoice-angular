6887e6ae2f2bda9bfaac92d70f489c1956ffb4a8
Julian Enrique <julian.buitrago3003@gmail.com>
2024-11-06 16:04:51 -0500

    feat: add user and agent endpoints

---
+++ src/app/service/agent.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DTOAgentResponse } from '../interfaces/agent/dto-agent-response';
+import { environment } from '../../environments/environment';
+import { DtoAgent } from '../interfaces/agent/dto-agent';
+import { DtoUserResponse } from '../interfaces/user/dto-user-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AgentService {
+  apiUrl = `${environment.apiBaseUrl}/users/`;
+
+  constructor(private http: HttpClient) {}
+
+  getAgentById(agentId: number): Observable<DTOAgentResponse> {
+    return this.http.get<DTOAgentResponse>(this.apiUrl + 'public/agents/' + agentId);
+  }
+
+  getAllAgents(): Observable<DtoUserResponse[]> {
+    return this.http.get<DtoUserResponse[]>(`${this.apiUrl}agents/`);
+  }
+
+  createAgent(agent: DtoAgent): Observable<DtoUserResponse> {
+    return this.http.post<DtoUserResponse>(`${this.apiUrl}agents/`, agent);
+  }
+
+  updateAgent(id: number, agent: DtoAgent): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}agents/${id}`, agent);
+  }
+
+  deleteAgent(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}agents/${id}`);
+  }
+}
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,18 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { DTOAgentResponse } from '../interfaces/user/dtoagent-response';
 import { environment } from '../../environments/environment';
+import { DtoUserResponse } from '../interfaces/user/dto-user-response';
+import { DtoUser } from '../interfaces/user/dto-user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  apiUrl = `${environment.apiBaseUrl}/users/`;
+  apiUrl = `${environment.apiBaseUrl}/users`;
 
   constructor(private http: HttpClient) {}
 
-  getAgentById(agentId: number): Observable<DTOAgentResponse> {
-    return this.http.get<DTOAgentResponse>(this.apiUrl + 'public/agents/' + agentId);
+  getAllUsers(): Observable<DtoUserResponse[]> {
+    return this.http.get<DtoUserResponse[]>(`${this.apiUrl}`);
+  }
+
+  getUserById(id: number): Observable<DtoUser> {
+    return this.http.get<DtoUser>(`${this.apiUrl}/${id}`);
+  }
+
+  createUser(user: DtoUser): Observable<DtoUserResponse> {
+    return this.http.post<DtoUserResponse>(`${this.apiUrl}`, user);
+  }
+
+  updateUser(id: number, user: DtoUser): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, user);
+  }
+
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
   }
 }
