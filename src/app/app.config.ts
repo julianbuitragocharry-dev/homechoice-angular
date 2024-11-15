d4eb64dd5934569daef95ed9285e5cb1abbd5054
Julian Enrique <julian.buitrago3003@gmail.com>
2024-11-15 15:57:12 -0500

    feat: add i18n configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, HttpClient } from '@angular/common/http';
 import { jwtInterceptor } from './auth/jwt.interceptor';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +16,14 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), 
     provideHttpClient(
       withInterceptors([jwtInterceptor])
-    )
+    ),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+    }))
   ]
 };
